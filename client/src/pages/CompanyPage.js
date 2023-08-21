6703e107a997015a4fbfc097071f656b8b781510
andrei-neculau4 <andreineculau4@gmail.com>
2023-08-21 16:26:03 -0400

    load jobs and companies with graphql

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,10 +1,38 @@
 import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { useEffect, useState } from 'react';
+import { getCompany } from '../lib/graphql/queries';
+import JobList from '../components/JobList';
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false
+  });
 
-  const company = companies.find((company) => company.id === companyId);
+  useEffect(() => {
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false});
+      }
+      catch(error) {
+        console.log('error', JSON.stringify(error, null, 2));
+        setState( { company: null, loading: false, error: true})
+      }
+    })();
+  }, [companyId]);
+
+  console.log('CompanyPage state:', state);
+  const { company, loading, error } = state;
+
+  if(loading){
+    return <div>Loading...</div>
+  }
+  if (error){
+    return <div className="has-text-danger">Data unavailable.</div>
+  }
   return (
     <div>
       <h1 className="title">
@@ -13,6 +41,10 @@ function CompanyPage() {
       <div className="box">
         {company.description}
       </div>
+      <h2 className="title is-5">
+        Jobs at ${company.name}
+      </h2>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,16 +1,40 @@
-import { getJobs } from './db/jobs.js';
+import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 export const resolvers = {
     Query: {
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+            if(!company) {
+                throw notFoundError('No company found with id ' + id);
+            }
+            return company;
+        },
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+            if(!job) {
+                throw notFoundError('No job found with id ' + id)
+            }
+            return job;
+        },
         jobs: () => getJobs(),
     },
 
+    Company: {
+        jobs: (company) => getJobsByCompany(company.id)
+    },
     Job: {
         date: (job) => toIsoDate(job.createdAt),
         company: (job) => getCompany(job.companyId)
     },
 };
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND'}
+    });
+}
 
 function toIsoDate(value) {
     return value.slice(0, 'yyyy-mm-dd'.length);
